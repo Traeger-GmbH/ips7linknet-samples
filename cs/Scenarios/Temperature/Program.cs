1f5fc0094a4ebe8bb3d9a23e9f94e3362f1a94c5
Dominik Scharnagl <48177425+dscharnagl@users.noreply.github.com>
2020-07-24 15:55:57 +0200

    Create Program.cs

---
+++ cs/Scenarios/Temperature/Program.cs
@@ -0,0 +1,28 @@
+namespace App
+{
+    using System;
+    using System.Threading;
+
+    using IPS7LnkNet.Advanced;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var device = new SiemensDevice(
+                    new IPDeviceEndPoint("192.168.0.80"),
+                    SiemensDeviceType.S71500);
+
+            using (var connection = device.CreateConnection()) {
+                connection.Open();
+
+                while (true) {                    
+                    var temperature = connection.ReadDouble("DB10.DBD 20");
+                    Console.WriteLine($"Current Temperature is {0} °C", temperature);
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
